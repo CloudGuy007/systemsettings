da04303b36e6fc4afcb03a86e30e9728765e062f
cameronwp <cameron.w.pittman@gmail.com>
2016-09-01 17:54:12 -0500

    adds lazy p
    Former-commit-id: b3a896edca369426e70b6f8231e58ae19d0dd676

--- zsh/base-aliases.zsh
+++ zsh/base-aliases.zsh
@@ -4,3 +4,4 @@ alias chrome="open -a 'Google Chrome'"
 alias gs='git status'
 alias ghlog='git log --all --graph --format="%C(yellow)%h %C(reset)%an %C(blue)%ar %C(red)%d %C(reset)%s"'
 alias resource='source ~/.bash_profile'
+alias p='python'
