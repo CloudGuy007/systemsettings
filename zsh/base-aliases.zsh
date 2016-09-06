3bce090b2f8fa9a6ddde0462112a67704fe32967
cameronwp <cameron.w.pittman@gmail.com>
2016-09-06 14:49:05 -0500

    adds make executable alias
    Former-commit-id: a6000ce931e221d79e885bae97804bb60ec9d1dd

--- zsh/base-aliases.zsh
+++ zsh/base-aliases.zsh
@@ -5,3 +5,5 @@ alias gs='git status'
 alias ghlog='git log --all --graph --format="%C(yellow)%h %C(reset)%an %C(blue)%ar %C(red)%d %C(reset)%s"'
 alias resource='source ~/.bash_profile'
 alias p='python'
+
+alias mkexe='chmod a+x'
