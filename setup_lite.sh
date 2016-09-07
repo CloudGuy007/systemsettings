f0b008c55f139adf5bc1706634169f05ad00087e
cameronwp <cameron.w.pittman@gmail.com>
2016-09-07 18:15:34 -0500

    adds setup script without vs code configs
    Former-commit-id: 29a0e7fe765cf73bb0680bf28ca1ec5ac7163561

---
+++ setup_lite.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+function link() {
+  SRC="$PWD/$1"
+  DST="$HOME/$2"
+  echo "Linking $DST -> $SRC"
+  ln -s -f "$SRC" "$DST"
+}
+
+# mkdir -p code/server
+
+# link common/serve.sh       code/server/serve.sh
+
+link common/bash_profile .bash_profile
+link common/zshrc .zshrc
+link zsh/base-aliases.zsh .oh-my-zsh/custom/base-aliases.zsh
+link zsh/git.zsh .oh-my-zsh/custom/git.zsh
+link zsh/marks.zsh .oh-my-zsh/custom/marks.zsh
+link zsh/themes/hand.zsh-theme .oh-my-zsh/custom/themes/hand.zsh-theme
