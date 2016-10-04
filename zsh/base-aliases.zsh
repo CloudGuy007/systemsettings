30924819947ed4a6cb96882894b48c44c4926065
cameronwp <cameron.w.pittman@gmail.com>
2016-10-03 20:53:02 -0400

    adds gcmsgx

--- zsh/base-aliases.zsh
+++ zsh/base-aliases.zsh
@@ -1,10 +1,10 @@
 alias ll="ls -al"
 alias chrome="open -a 'Google Chrome'"
-
-alias gs='git status'
+alias gcmsgx='git commit --no-verify -m'
 alias ghlog='git log --all --graph --format="%C(yellow)%h %C(reset)%an %C(blue)%ar %C(red)%d %C(reset)%s"'
-alias resource='source ~/.bash_profile'
+alias gs='git status'
 alias p='python'
+alias resource='source ~/.bash_profile'
 
 function mkexe() {
   touch $1;
