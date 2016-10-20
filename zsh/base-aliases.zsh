4a45715793ec96e4ca11c631eb6db1f39026ad41
cameronwp <cameron.w.pittman@gmail.com>
2016-10-20 14:43:54 -0700

    updates aliases

--- zsh/base-aliases.zsh
+++ zsh/base-aliases.zsh
@@ -2,6 +2,7 @@ alias ll="ls -al"
 alias chrome="open -a 'Google Chrome'"
 alias gcmsgx='git commit --no-verify -m'
 alias gcamx='git commit -a --no-verify -m'
+alias gcnx!="git commit --amend --no-edit --no-verify"
 alias ghlog='git log --all --graph --format="%C(yellow)%h %C(reset)%an %C(blue)%ar %C(red)%d %C(reset)%s"'
 alias gs='git status'
 alias p='python'
