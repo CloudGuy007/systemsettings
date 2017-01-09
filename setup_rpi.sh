3af68649f11fbb25202a5a10ba8d9ca2b4e3c0fa
cameronwp <cameron.w.pittman@gmail.com>
2017-01-09 08:40:31 -0600

    improves ubuntu setup

--- setup_rpi.sh
+++ setup_rpi.sh
@@ -17,11 +17,9 @@ link common/bash_profile .bash_profile
 link common/zshrc .zshrc
 link common/vimrc .vimrc
 link zsh/base-aliases.zsh .oh-my-zsh/custom/base-aliases.zsh
-link zsh/ubuntu-aliases.zsh .oh-my-zsh/custom/ubuntu-aliases.zsh
 link zsh/git.zsh .oh-my-zsh/custom/git.zsh
 link zsh/marks.zsh .oh-my-zsh/custom/marks.zsh
 link zsh/themes/hand.zsh-theme .oh-my-zsh/custom/themes/hand.zsh-theme
-link code/settings.json .config/Code/User/settings.json
-link code/keybindings-ubuntu.json .config/Code/User/keybindings.json
+link zsh/rpi-aliases.zsh .oh-my-zsh/custom/rpi-aliases.zsh
 
 source ~/.bash_profile
